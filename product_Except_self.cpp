64f9415c7a5a477bd41dde8dd6b4d14a12c642ae
himaniaggarwal2 <himaniaggarwal2@gmail.com>
2021-02-28 19:06:02 +0530

    * One way of solving Product_except_self with division

---
+++ product_Except_self.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int p=1;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0) p= p*nums[i];
+            else count++;
+        }//product of whole nums array except the point when it is zero
+        // zero should be edge case
+        for(int i=0;i<nums.length;i++){
+            if(count>=2) nums[i]=0; //more than 2 zeros in array nums
+            //it means that even if we don't include self vaala zero there is gonna be zero somewhere else
+            //and that would lead to automatic zero of the whole array
+            else{
+                if(nums[i]==0) nums[i]=p;
+                else{
+                    if(count>0) nums[i]=0;
+                    else nums[i]=p/nums[i];
+                    }
+                }     
+        }
+        return nums;
+    }
+}
