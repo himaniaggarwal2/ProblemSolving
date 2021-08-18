6752e9fa39d756445bdd5be360c998edd80457f0
himaniaggarwal2 <himaniaggarwal2@gmail.com>
2021-08-18 20:23:01 +0530

    for loop mistake detected and corrected

--- nextGreaterElement.cpp
+++ nextGreaterElement.cpp
@@ -4,29 +4,34 @@ using namespace std;
 int main(){
     vector<long long> arr={1 ,3, 2, 4};
     int n=4;
-    vector<long long> v;
-        stack<long long> st;
-        for(int i=n-1;i>=0;i++){
-            if(st.size()==0){
-                v.push_back(-1);
-            }
-            else if(st.size() > 0 && st.top()>arr[i]){    
-                v.push_back(st.top());
-            }
-            else if(st.size()>0 && st.top()<=arr[i]){
-                while(st.size()>0 && st.top()<=arr[i]){
-                    st.pop();
-                }
-                if(st.size()==0){
-                    v.push_back(-1);
-                }
-                else{
-                    v.push_back(st.top());
-                }
-            }
-            st.push(arr[i]);
+     vector<long long> v; 
+    stack <long long> s; 
+    for (int i=n-1;i>=0;i--){
+      if(s.size() ==0) 
+        v.push_back(-1);
+      else if (s.size()>0 && s.top()>arr[i])  
+      {
+        v.push_back(s.top());  
+      }
+      else if (s.size()>0 && s.top()<=arr[i]) 
+      {
+        while(s.size()>0 && s.top()<=arr[i]) 
+        {
+          s.pop(); 
         }
-        std::reverse(v.begin(),v.end());
+        if (s.size()==0) 
+        {
+          v.push_back(-1);
+        }
+        else
+        {
+          v.push_back(s.top());  
+        }
+      }
+      s.push(arr[i]); 
+    }
+    reverse(v.begin(),v.end()); 
+    
         for(int i=0;i<v.size();i++){
             cout<<" "<<v[i]<<endl;
         }
