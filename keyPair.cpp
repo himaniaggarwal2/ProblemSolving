f85ac5d763d53b74e9e643399d4bbb9e2ef943fe
himaniaggarwal2 <himaniaggarwal2@gmail.com>
2021-08-24 23:30:30 +0530

    changes made

--- keyPair.cpp
+++ keyPair.cpp
@@ -1,14 +1,11 @@
 // https://practice.geeksforgeeks.org/problems/key-pair5616/1
 
 bool hasArrayTwoCandidates(int arr[], int n, int x) {
-	    set<int> st;
-	    
-	    for(int i=0;i<n;i++){
-	        if(st.find(x-arr[i])!=st.end()){
-	        return true;
-	            
-	        }
-	        st.insert(arr[i]);
-	    }
-	    return false;
+	    sort(arr, arr+size);
+    for(int i=0; i<size; i++){
+        int big_num=n+arr[i];
+        bool found=binary_search(arr+i, arr+size, big_num);
+        if(found) return true;
+    }
+    return false;
 	}
